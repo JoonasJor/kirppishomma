3ee6b9808ea3b5a6b0c4992f5059a739d43c4371
bobtheburgerflipper <t9jojo01@students.oamk.fi>
2021-11-22 19:27:27 +0200

    tuotesivu

---
+++ app/src/main/java/com/example/kirppis/ItemListing.java
@@ -0,0 +1,24 @@
+package com.example.kirppis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class ItemListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_listing);
+
+        TextView textItemId = findViewById(R.id.textViewItemId);
+
+        Intent intent = getIntent();
+        String itemId = intent.getStringExtra("item id");
+        textItemId.setText(itemId);
+        Log.d("item id", itemId);
+    }
+}
--- app/src/main/java/com/example/kirppis/MainActivity.java
+++ app/src/main/java/com/example/kirppis/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void GoToShoppingCart(View view){
         Intent intent = new Intent(this, ShoppingCart.class);
+        startActivity(intent);
+    }
+    public void GoToItemListing(View view){
+        Intent intent = new Intent(this, ItemListing.class);
+        intent.putExtra("item id", String.valueOf(view.getTag()));
         startActivity(intent);
     }
 }
